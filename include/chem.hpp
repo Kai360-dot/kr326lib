76026a3f41ae5f04e8e2172fd6375a4a3a8ab829
Kai Ruth <contact@kairuth.ch>
2026-06-19 16:39:40 +0100

    update chem.cpp add chem.hpp

---
+++ include/chem.hpp
@@ -0,0 +1,9 @@
+#ifndef CHEM_HPP
+#define CHEM_HPP
+
+// Universal gas constant
+extern double R_GAS; // (J / (mol*K)).
+
+double get_P_ideal_gas(double n, double T, double V);
+
+#endif // CHEM_HPP
--- src/chem.cpp
+++ src/chem.cpp
@@ -1,3 +1,5 @@
+#include "chem.hpp"
+
 double R_GAS = 8.314; // create constants file
 
 double get_P_ideal_gas(double n, double T, double V)
