bac06c09a48859b453f3c2fed00915f73c3c2933
Kai Ruth <contact@kairuth.ch>
2026-06-19 16:48:04 +0100

    add python bindings

---
+++ src/bindings.cpp
@@ -0,0 +1,9 @@
+#include <pybind11/cast.h>
+#include <pybind11/pybind11.h>
+#include <chem.hpp>
+
+PYBIND11_MODULE(kr326lib, m){
+  m.doc() = "pybind11 module for kr326lib by Kai Ruth (ETH Zurich, Imperial)";
+  m.def("add", &get_P_ideal_gas, "Compute P from ideal gas equation", 
+        pybind11::arg("n"), pybind11::arg("T"), pybind11::arg("V"));
+}
