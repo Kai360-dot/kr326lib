9ceb73ed9ca763a1cd8a08056d49c049e2a9b6c9
Kai Ruth <contact@kairuth.ch>
2026-06-20 11:42:59 +0100

    add uv.lock fix formatting

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -4,6 +4,6 @@
 
 PYBIND11_MODULE(kr326lib, m){
   m.doc() = "pybind11 module for kr326lib by Kai Ruth (ETH Zurich, Imperial)";
-  m.def("get_P_ideal_gas", &get_P_ideal_gas, "Compute P from ideal gas equation", 
+  m.def("get_P_ideal_gas", &get_P_ideal_gas, "Compute P from ideal gas equation",
         pybind11::arg("n"), pybind11::arg("T"), pybind11::arg("V"));
 }
